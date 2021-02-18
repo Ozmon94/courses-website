8aac3b5cc066657775c283eea563025023818a4b
Ozmon94 <szymonmoskwa@gmail.com>
2021-02-18 12:17:47 +0100

    Implement app store and request helper

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import StoreProvider from '../store/StoreProvider'
+
+
+function App() {
+  return (
+    <StoreProvider>
+    <div className="App">
+      <h3>elo ziemnia </h3>
+    </div>
+    </StoreProvider>
+  );
+}
+
+export default App;
